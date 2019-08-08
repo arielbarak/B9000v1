e7e595483a8fa9182e7fc7c7e9ab0c034ac1825a
ArielB <arieli.barak@gmail.com>
2019-08-08 14:21:12 +0300

    -side slide navigation need to slide on click -i think i need to remove "ImageViewAdapter" and just use the regular main feed thingy(that i need to do) and inject only relevant posts; -layout image view? -design! -add db work -anything else before initializing firebase firestore?(db)

--- app/src/main/java/com/example/b9000v1/MainActivity.java
+++ app/src/main/java/com/example/b9000v1/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.b9000v1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this, R.id.main_nav_host);
+
+        BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
+
+        navView.setOnNavigationItemSelectedListener( menuItem -> {
+            navController.navigate(menuItem.getItemId());
+            return true;
+        });
+
     }
 }
